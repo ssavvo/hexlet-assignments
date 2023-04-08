1a6928bc7022ee2194ac58d995d479168b3a527e
@hexlet/cli <support@hexlet.io>
2023-04-08 22:10:24 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,13 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// BEGIN
+class App {
+    public static List<List<String>> enlargeArrayImage(List<List<String>> image) {
+        List<List<String>> enlargedImage = new ArrayList<>(new ArrayList<>());
+    }
+}
+// END
