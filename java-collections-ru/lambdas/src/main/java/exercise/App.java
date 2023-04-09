faf44a0188ce0f3a16a7332b93536136d18060dd
@hexlet/cli <support@hexlet.io>
2023-04-09 03:51:45 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -8,6 +8,16 @@ import java.util.List;
 class App {
     public static List<List<String>> enlargeArrayImage(List<List<String>> image) {
         List<List<String>> enlargedImage = new ArrayList<>(new ArrayList<>());
+        for (List<String> row : image) {
+            List<String> enlargedRow = new ArrayList<>();
+            for (String s : row) {
+                enlargedRow.add(s);
+                enlargedRow.add(s);
+            }
+            enlargedImage.add(enlargedRow);
+            enlargedImage.add(enlargedRow);
+        }
+        return enlargedImage;
     }
 }
 // END
--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -1,8 +1,34 @@
 package exercise;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
-
+class AppTest {
+    @Test
+    void testEnlargeEmptyList() {
+        List<List<String>> image = List.of();
+        List<List<String>> actual = App.enlargeArrayImage(image);
+        List<List<String>> expected = List.of();
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void testEnlargeSquareList() {
+        List<List<String>> image = List.of(List.of("*", "*", "*"), List.of("*", " ", "*"), List.of("*", "*", "*"));
+        List<List<String>> actual = App.enlargeArrayImage(image);
+        List<List<String>> expected = List.of(List.of("*", "*", "*", "*", "*", "*"), List.of("*", "*", "*", "*", "*", "*"), List.of("*", "*", " ", " ", "*", "*"), List.of("*", "*", " ", " ", "*", "*"), List.of("*", "*", "*", "*", "*", "*"), List.of("*", "*", "*", "*", "*", "*"));
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    void testEnlargeRectList() {
+        List<List<String>> image = List.of(List.of("*", "*", "*"), List.of("*", "*", "*"));
+        List<List<String>> actual = App.enlargeArrayImage(image);
+        List<List<String>> expected = List.of(List.of("*", "*", "*", "*", "*", "*"), List.of("*", "*", "*", "*", "*", "*"), List.of("*", "*", "*", "*", "*", "*"), List.of("*", "*", "*", "*", "*", "*"));
+        assertThat(actual).isEqualTo(expected);
+    }
+}
 // END
